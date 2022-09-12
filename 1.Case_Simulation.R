be73f6ff3d898827cc38fcd0118635ef4bcf95ba
Alistair McNair Senior <41315906+AlistairMcNairSenior@users.noreply.github.com>
2022-09-12 10:04:44 +1000

    Update 1.Case_Simulation.R
    Adjusting for Proofs

--- 1.Case_Simulation.R
+++ 1.Case_Simulation.R
@@ -10,12 +10,12 @@ library(coxme)
 library(survminer)
 library(mvtnorm)
 library(doSNOW)
-
+library(ggplot2)
 
 # Set the wd
 setwd("/Users/alistairsenior/OneDrive - The University of Sydney (Staff)/Simulation Genetic Mapping/Simulation_Submitted")
 
-# Function to simulate survival data for a GxE experiment. 1 treatment and 1 control group, where effect size in lnHR. Takes assemend G sd in intercepts and treatments, and corr_IT. Specify n-strains and n-per group. k is number of datasets with exactly same simualted genetic effects. default of 1. Output is a list of length k + 1, where the first k are the datasets and the last is the G-effects sampled
+# Function to simulate survival data for a GxE experiment. 1 treatment and 1 control group, where effect size in lnHR. Takes assumed G sd in intercepts and treatments, and corr_IT. Specify n-strains and n-per group. k is number of datasets with exactly same simualted genetic effects. default of 1. Output is a list of length k + 1, where the first k are the datasets and the last is the G-effects sampled
   
 # Data are simulated assuming Gompertz. What to take as the lambda (scale) and gamma (shape)? Simons et al. perform a quasi meta-analysis of DR in rodents giving the ln(a) = -11.57 and ln(b) = -4.90, where a * exp(b * t) is their equation - these are defaults for a and b
 
@@ -69,14 +69,16 @@ fit<-survfit(Surv(eventtime/7, status) ~ trts, data=dat)
 # Plot a boxplot + violin
 pdf("F1.A.pdf", height=8.2/3, width=8.2/3)
 p1<-ggplot(dat, aes(x=eventtime/7, col=diet, fill=diet)) +
-	geom_density(alpha=0.2) + theme_bw() + xlab("Age at Death (Weeks)") + labs(title="A") + ylab("Density") +
-	theme(legend.position=c(0.23, 0.7)) + xlim(0, 200)
+	geom_density(alpha=0.2) + theme_bw() + xlab("Age at Death (wks)") + labs(title="A") + ylab("Density") +
+	theme(legend.position=c(0.23, 0.7), text=element_text(size=15)) + xlim(0, 200) +
+	scale_colour_manual(values=c("black", "cornflowerblue")) +
+	scale_fill_manual(values=c("black", "cornflowerblue"))
 print(p1)
 dev.off()
 
 # plot a KM
 pdf("F1.B.pdf", height=8.2/3, width=8.2/3)
-p2<-ggsurvplot(fit, legend="none", censor=F, title="B", xlab = "Time (Weeks)", ylab="Survivorship")
+p2<-ggsurvplot(fit, legend="none", censor=F, title="B", xlab = "Time (wks)", ylab="Survivorship", font.x=15, font.y=15, font.title=18, palette=c("black", "cornflowerblue"))
 print(p2)
 dev.off()
 
@@ -98,13 +100,14 @@ mode[order(mode$..1),]
 # Add in a histrogram of the mortality data in mice.
 Rikke_data<-read.csv("Rikke.csv")
 pdf("F1.C.pdf", height=8.2/3, width=8.2/3)
-p3<-ggplot(Rikke_data, aes(x=Lifespan/7)) + geom_histogram() + xlab("Age at Death (Weeks)") + labs(title="C") + theme_bw() + ylab("Count")
+p3<-ggplot(Rikke_data, aes(x=Lifespan/7)) + geom_histogram() + xlab("Age at Death (wks)") + labs(title="C") + theme_bw() + ylab("Count") + theme(text=element_text(size=15))
 print(p3)
 dev.off()
 
 # Lets simulate some strains, but assume no genetic variance
 set.seed(567)
-dat<-my_sim(n_strain=40, n=5, lnHR=-0.5, sigma_IntG=0, sigma_Tr=0, cor_IntTrt=0)[[1]]
+experiment<-my_sim(n_strain=40, n=5, lnHR=-0.5, sigma_IntG=0, sigma_Tr=0, cor_IntTrt=0, k=2)
+dat<-experiment[[1]]
 dat$trts_cat<-as.factor(dat$trts)
 
 # Now get the strain by strain effect sizes - calculated as lm (t-tests, basically)
@@ -124,16 +127,16 @@ range(effects$ES)
 # Make waterfall plots
 pdf("F1.D.pdf", height=8.2/3, width=6)
 p3<-ggplot(effects, aes(x=seq(1, 40, 1), y=ES)) + geom_point() + theme_bw() +
-	theme(axis.text.x=element_text(angle=90, size=10), axis.ticks.x=element_blank(), panel.grid.minor=element_blank()) + 
-	labs(title="D", x = "Strain ID", y = "Diff. Lifespan [DR - AL] - LM") + 
-	geom_hline(yintercept=0) + ylim(-300, 300) + 
-	annotate("text", x=seq(1, 40, 1), y=280, label=effects$signif, size=10) + 
+	theme(axis.text.x=element_text(angle=90, size=11), axis.ticks.x=element_blank(), panel.grid.minor=element_blank(), text=element_text(size=15)) + 
+	labs(title="D", x = "Strain ID", y = "Diff. Lifespan [DR - AL]") + 
+	geom_hline(yintercept=0) + ylim(-400, 400) + 
+	annotate("text", x=seq(1, 40, 1), y=365, label=effects$signif, size=10) + 
 	scale_x_continuous(breaks=seq(1, 40, 1), labels=effects$strains)
 print(p3)
 dev.off()
 
-# Let's repeat to see what happens
-dat<-my_sim(n_strain=40, n=5, lnHR=-0.5, sigma_IntG=0, sigma_Tr=0, cor_IntTrt=0)[[1]]
+# Let's look at second experiment to see what happens
+dat<-experiment[[2]]
 
 # Now get the strain by strain effect sizes - calculated as lm and coxph
 effects<-data.frame(strains = seq(1, 40, 1), ES=0, SE=0, p=0, ES_hr=0)
@@ -163,31 +166,31 @@ lmm<-lmer(eventtime ~ diet + (1+diet|strain), data=dat)
 summary(lmm)
 # Random effects:
  # Groups   Name        Variance Std.Dev. Corr 
- # strain   (Intercept)  1182.6   34.39        
-          # dietDR        988.5   31.44   -1.00
- # Residual             22465.5  149.88        
+ # strain   (Intercept)  2962     54.42        
+          # dietDR       1825     42.72   -1.00
+ # Residual             28716    169.46        
 # Number of obs: 400, groups:  strain, 40
 
 # Fixed effects:
             # Estimate Std. Error     df t value Pr(>|t|)    
-# (Intercept)   820.53      11.91  39.30  68.883  < 2e-16 ***
-# dietDR         86.39      15.79  93.14   5.471 3.76e-07 ***
-31.44 / 86.39
+# (Intercept)   805.79      14.75  39.75  54.622  < 2e-16 ***
+# dietDR         90.83      18.24  75.38   4.979 3.93e-06 ***
+42.72 / 90.83
 
 
 # What if we use coxme - the right model AND shrinkage
 coxmm<-coxme(Surv(eventtime, status) ~ trts + (1+trts|strain), data=dat)
 summary(coxmm)
 # Fixed coefficients
-           # coef exp(coef)  se(coef)    z       p
-# trts -0.5882912 0.5552754 0.1032758 -5.7 1.2e-08
+           # coef exp(coef)  se(coef)     z       p
+# trts -0.6320796 0.5314854 0.1043752 -6.06 1.4e-09
 
 # Random effects
  # Group  Variable  Std Dev       Variance      Corr         
- # strain Intercept  8.188634e-03  6.705372e-05 -8.394434e-01
-        # trts       6.865365e-03  4.713324e-05              
-
-8.188634e-03 / 0.5882912
+ # strain Intercept  7.112468e-03  5.058720e-05 -8.691514e-01
+        # trts       8.840263e-03  7.815025e-05              
+              
+ 8.840263e-03 / 0.6320796
 
 # Extract the estimates from lmm and coxme and add to the effects for plotting
 lmm_ES<-ranef(lmm)$strain
@@ -198,13 +201,13 @@ effects$cox<-cox_ES[match(effects$strains, row.names(cox_ES)), 2] + coxmm$coeff
 # Make waterfall plots
 pdf("F1.E.pdf", height=8.2/3, width=6)
 p4<-ggplot(effects, aes(x=seq(1, 40, 1), y=ES)) + geom_point() + theme_bw() +
-	theme(axis.text.x=element_text(angle=90, size=10), axis.ticks.x=element_blank(), panel.grid.minor=element_blank()) + 
+	theme(axis.text.x=element_text(angle=90, size=11), axis.ticks.x=element_blank(), panel.grid.minor=element_blank(), text=element_text(size=15)) + 
 	labs(title="E", x = "Strain ID", y = "MD Lifespan [DR - AL]") + 
 	geom_hline(yintercept=0) +
-	annotate("text", x=seq(1, 40, 1), y=280, label=effects$signif, size=10) + 
+	annotate("text", x=seq(1, 40, 1), y=365, label=effects$signif, size=10) + 
 	scale_x_continuous(breaks=seq(1, 40, 1), labels=effects$strains)	 +
 	geom_point(aes(x=seq(1, 40, 1), y=lmm), inherit.aes=F, col="cornflowerblue") +
-	scale_y_continuous(limits=c(-300, 300))
+	scale_y_continuous(limits=c(-400, 400))
 print(p4)
 dev.off()
 
@@ -213,7 +216,7 @@ dev.off()
 effects<-effects[order(effects$ES_hr),]
 pdf("F1.F.pdf", height=8.2/3, width=6)
 p5<-ggplot(effects, aes(x=seq(1, 40, 1), y=ES_hr)) + geom_point() + theme_bw() +
-	theme(axis.text.x=element_text(angle=90, size=10), axis.ticks.x=element_blank(), panel.grid.minor=element_blank()) + 
+	theme(axis.text.x=element_text(angle=90, size=11), axis.ticks.x=element_blank(), panel.grid.minor=element_blank(), text=element_text(size=15)) + 
 	labs(title="F", x = "Strain ID", y = "lnHR [DR / AL]") + 
 	geom_hline(yintercept=0) +
 	scale_x_continuous(breaks=seq(1, 40, 1), labels=effects$strains) + 
